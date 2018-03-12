f62e9798bf5f196cceb5d249439308ecd8783676
Szyman <szyman040395@gmail.com>
2018-03-12 14:46:31 +0100

    base layout [WiP]

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $upcomin_events = [];
+
+        return view('home', compact('upcomin_events'));
+    }
+}
