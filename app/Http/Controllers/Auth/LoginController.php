1493f1098faa024e311b11180dd2e4ea9cbeb9a8
Szyman <szyman040395@gmail.com>
2018-03-16 13:27:46 +0100

    Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Symfony\Component\HttpFoundation\Request;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -35,5 +36,28 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function rules()
+    {
+        return [
+           'login' => 'required',
+           'password' => 'required'
+        ];
+    }
+
+    public function login(Request $request)
+    {
+        $field = filter_var($request->input('login'), FILTER_VALIDATE_EMAIL) ? 'email' : 'nick';
+        $request->merge([$field => $request->input('login')]);
+
+        if (\Auth::attempt($request->only($field, 'password')))
+        {
+            return redirect('/');
+        }
+
+        return redirect('/login')->withErrors([
+            'error' => 'These credentials do not match our records.',
+        ]);
     }
 }
